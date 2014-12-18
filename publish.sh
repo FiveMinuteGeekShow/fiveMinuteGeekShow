46daa735e39787646d7b5a58a79d24cc919706c6
Matt Stauffer <matt@tighten.co>
2014-12-18 01:09:38 -0500

    Tweak pubish script and git ignore.

--- publish.sh
+++ publish.sh
@@ -7,5 +7,11 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
-if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+# rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+
+# todo: wipe
+cp -r output_prod/* ../fiveMinuteGeekShowPublic/
+cd ../fiveMinuteGeekShowPublic
+git add .
+git commit -m "Build."
+git push origin master
